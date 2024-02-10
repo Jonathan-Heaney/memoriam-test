fb0657216f1f34dd9c29bc44762df595d8fb5a1b
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-02-10 12:32:51 -0600

    Registered models

--- emailsender/admin.py
+++ emailsender/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Subscriber, Quote, QuotesSent, SubscriptionYear
+
+admin.site.register(Subscriber)
+admin.site.register(Quote)
+admin.site.register(QuotesSent)
+admin.site.register(SubscriptionYear)
